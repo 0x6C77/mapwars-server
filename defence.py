acacb76c16e377a2c61863bd6149df35437a61a7
flabbyrabbit <flabbyrabbit@gmail.com>
2013-03-04 22:00:30 +0000

    Added support for defence and vehicles. Started work on SQLite database

---
+++ defence.py
@@ -0,0 +1,8 @@
+from unit import Unit
+
+print "Loaded defence class..."
+
+class Defence(Unit):
+	def check_surroundings(self):
+		self.attack = true
+
---
+++ vehicle.py
@@ -0,0 +1,7 @@
+from unit import Unit
+
+print "Loaded vehicle class..."
+
+class Vehicle(Unit):
+	def set_stance(self, stance):
+		self.stance = stance
